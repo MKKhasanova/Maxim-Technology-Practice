48655659b2760cccadc025f5c23b5f1f2fb86775
Mariyam Khasanova <mariyam9916@icloud.com>
2025-04-30 10:18:58 +0300

    выполнено задание 5

--- TASK/obj/Debug/net8.0/TASK.AssemblyInfo.cs
+++ TASK/obj/Debug/net8.0/TASK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TASK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83318c87399568c46500bd75a954e8d210696e3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12900974581cf1e7692f050d8ef59c1a1a30ad24")]
 [assembly: System.Reflection.AssemblyProductAttribute("TASK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TASK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
