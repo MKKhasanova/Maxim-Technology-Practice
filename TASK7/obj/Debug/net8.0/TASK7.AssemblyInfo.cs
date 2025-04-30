2350cc24ce7d24e8b24a00ca7ac05785a215da4b
Mariyam Khasanova <mariyam9916@icloud.com>
2025-04-30 23:59:37 +0300

    выполнено 8

--- TASK7/obj/Debug/net8.0/TASK7.AssemblyInfo.cs
+++ TASK7/obj/Debug/net8.0/TASK7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TASK7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac451d13435c9905197a67a0422fbe6f7211101c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TASK7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TASK7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
